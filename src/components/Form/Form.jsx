1e24f14b0d34023d2e716ccb52d995af1f4b39f9
kmthein <kmthein96.mm@gmail.com>
2023-05-12 22:56:49 +0630

    useRef update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Form = ({addNewEmp}) => {
-  const [empData, setEmpData] = useState({
-    id: '',
-    name: '',
-    age: ''
-  })
+  // const [empData, setEmpData] = useState({
+  //   id: '',
+  //   name: '',
+  //   age: ''
+  // })
+  
+  // const idOnChange = (event) => {
+  //   setEmpData({
+  //     ...empData,
+  //     id: event.target.value
+  //   })
+  // }
 
-  const idOnChange = (event) => {
-    setEmpData({
-      ...empData,
-      id: event.target.value
-    })
-  }
+  // const nameOnChange = (event) => {
+  //   setEmpData({
+  //     ...empData,
+  //     name: event.target.value
+  //   })
+  // }
 
-  const nameOnChange = (event) => {
-    setEmpData({
-      ...empData,
-      name: event.target.value
-    })
-  }
+  // const ageOnChange = (event) => {
+  //   setEmpData({
+  //     ...empData,
+  //     age: event.target.value
+  //   })
+  // }
 
-  const ageOnChange = (event) => {
-    setEmpData({
-      ...empData,
-      age: event.target.value
-    })
-  }
+  const idChangeRef = useRef();
+  const nameChangeRef = useRef();
+  const ageChangeRef = useRef();
 
   const submitData = (event) => {
     event.preventDefault();
+    const empData = {
+      id: idChangeRef.current.value,
+      name: nameChangeRef.current.value,
+      age: ageChangeRef.current.value
+    }
     addNewEmp(empData);
-    setEmpData({
-      id: '',
-      name: '',
-      age: ''
-    })
+    idChangeRef.current.value = "";
+    nameChangeRef.current.value = "";
+    ageChangeRef.current.value = "";
   }
   return (
     <div>
         <form onSubmit={submitData}>  
-          <input type="text" placeholder='ID' onChange={idOnChange} value={empData.id} required/>
-          <input type="text" placeholder='Name' onChange={nameOnChange} value={empData.name} required/>
-          <input type="text" placeholder='Age' onChange={ageOnChange} value={empData.age} required/>
+          <input type="number" placeholder='ID' ref={idChangeRef} required/>
+          <input type="text" placeholder='Name' ref={nameChangeRef} required/>
+          <input type="number" placeholder='Age' ref={ageChangeRef} required/>
             <button className='submit-btn'>Submit</button>
         </form>
     </div>
